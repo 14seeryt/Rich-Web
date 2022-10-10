c2239eb45cee6ffd6a649efe8ddbdf280883f7cc
14seeryt <57757387+14seeryt@users.noreply.github.com>
2022-10-10 21:40:19 +0100

    Newest Commit
    Most recent commit

---
+++ Lab 1/ReadME.txt
@@ -0,0 +1,8 @@
+https://github.com/14seeryt/Rich-Web.git
+
+Question 3 - My Extension is made to inform the user that the webpage they are attempting to access is 
+             unavailable to minorly inconvenience them throughout their use of their browser. It replaces all images on 
+             screen with an array of 5 selected error message images. It achievs this by storing the images as links in an array.
+             This array is then reversed through and a random image is picked and then replaces an image on the visted webpage.
+             Each header element is replaced by text that tells the user errors are awesome as well paragraphs being replaced with another error message.
+             Finally a .alert() message appears if there is an input box present informing the user that the webpage no longer works.
--- Lab 1/problem-3/Extension/Content.js
+++ Lab 1/problem-3/Extension/Content.js
@@ -1,6 +1,9 @@
 //array of images
 let errorMessage = [
-    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freecodecamp.org%2Fnews%2Fhow-to-write-helpful-error-messages-to-improve-your-apps-ux%2F&psig=AOvVaw2h5hX7Z-YVXqFPr2DPYGwa&ust=1665477333804000&source=images&cd=vfe&ved=2ahUKEwjSjZbYoNX6AhULIsAKHZlgCSsQr4kDegUIARDbAQ"
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freecodecamp.org%2Fnews%2Fhow-to-write-helpful-error-messages-to-improve-your-apps-ux%2F&psig=AOvVaw2h5hX7Z-YVXqFPr2DPYGwa&ust=1665477333804000&source=images&cd=vfe&ved=2ahUKEwjSjZbYoNX6AhULIsAKHZlgCSsQr4kDegUIARDbAQ",
+    "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.elegantthemes.com%2Fblog%2Fwp-content%2Fuploads%2F2020%2F08%2F000-http-error-codes.png&imgrefurl=https%3A%2F%2Fwww.elegantthemes.com%2Fblog%2Fwordpress%2Fthe-ultimate-guide-to-common-http-error-codes&tbnid=Leystw72I66tAM&vet=12ahUKEwiH4ontutb6AhXvTUEAHQC4DkEQMygAegUIARDbAQ..i&docid=G84NVnYGVQ80kM&w=1080&h=608&q=error&ved=2ahUKEwiH4ontutb6AhXvTUEAHQC4DkEQMygAegUIARDbAQ",
+    "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.computerhope.com%2Fjargon%2Fe%2Ferror.png&imgrefurl=https%3A%2F%2Fwww.computerhope.com%2Fjargon%2Fe%2Ferror.htm&tbnid=cAx03INsmFse5M&vet=12ahUKEwiH4ontutb6AhXvTUEAHQC4DkEQMygBegUIARDdAQ..i&docid=Z0ChwtQ5SLVbDM&w=350&h=350&q=error&ved=2ahUKEwiH4ontutb6AhXvTUEAHQC4DkEQMygBegUIARDdAQ",
+    "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.itpro.co.uk%2Fimage%2Fupload%2Fs--X-WVjvBW--%2Ff_auto%2Ct_content-image-full-desktop%401%2Fv1570814016%2Fitpro%2Fblue_screen_of_death.png&imgrefurl=https%3A%2F%2Fwww.itpro.com%2Foperating-systems%2Fmicrosoft-windows%2F358097%2Fhow-to-fix-a-blue-screen-of-death-error-in-windows-10&tbnid=xQ0IjZ2-vynuFM&vet=12ahUKEwiH4ontutb6AhXvTUEAHQC4DkEQMygFegUIARDmAQ..i&docid=xmD8dWcFVNYIxM&w=1260&h=710&q=error&ved=2ahUKEwiH4ontutb6AhXvTUEAHQC4DkEQMygFegUIARDmAQ"
 ];
 
 const imgs = document.getElementsByTagName("img");
@@ -17,7 +20,12 @@ for (let i = 0; i < 10; i++) {
 //do the same for footer elements
 const footer = document.getElementsByTagName("footer");
 for (let i = 0; i < footer.length; i++) {
-    footer[i].innerText = "This webpage is broken!";
+    footer[i].innerText = "This webpage is no longer working !";
+}
+
+const paragraph = document.getElementsByTagName("p");
+for (let i = 0; i < paragraph.length; i++) {
+    paragraph[i].innerText = "This webpage is broken!";
 }
 
 
